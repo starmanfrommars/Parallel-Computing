7f0a570e6ad699cc4db497d7b10cb2d3f5d38e27
Starman <starmanfrommars5@gmail.com>
2025-09-16 11:34:09 +0000

    Added Lab 1

---
+++ Lab_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(){
+    int n;
+    printf("Enter the number of iterations : ");
+    scanf("%d",&n);
+
+    #pragma omp parallel for schedule(static,2)
+    for(int i=0;i<n;i++){
+        int tid = omp_get_thread_num();
+        printf("Thread no. %d is executing\n", tid);
+    }
+    return 0;
+}
+
+// gcc Program1.c -fopenmp
