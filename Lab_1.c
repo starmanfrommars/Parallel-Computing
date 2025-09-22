6aaa313d58fafd71207009bffcbf6c98724b1788
Starman <starmanfrommars5@gmail.com>
2025-09-22 19:32:08 +0000

    Added Lab 1

--- Lab_1.c
+++ Lab_1.c
@@ -9,7 +9,7 @@ int main(){
     #pragma omp parallel for schedule(static,2)
     for(int i=0;i<n;i++){
         int tid = omp_get_thread_num();
-        printf("Thread no. %d is executing\n", tid);
+        printf("Thread no. %d is executing iteration %d\n", tid,i);
     }
     return 0;
 }
